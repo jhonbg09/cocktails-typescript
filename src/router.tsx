96fa72797908e9102fff57aaa16fd90db73a1aee
jhonbg09 <jhon.betancourthg09@gmail.com>
2024-10-04 17:11:50 -0500

    Optimizando proyecto

--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,23 @@
+import { lazy, Suspense } from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import IndexPages from "./pages/IndexPages";
-import FavoritesPages from "./pages/FavoritesPages";
+
 import Layout from "./layouts/Layout";
+
+
+const FavoritesPage = lazy(()=>import("./pages/FavoritesPages")) 
+
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<IndexPages/>} index/> //el atributo index indica que es la pagina pincipal
-          <Route path="/favoritos" element={<FavoritesPages />} />
+          <Route path="/favoritos" element={
+            <Suspense fallback="Cargando...">
+              <FavoritesPage/>
+            </Suspense>
+            }/>
         </Route>
       </Routes>
     </BrowserRouter>
